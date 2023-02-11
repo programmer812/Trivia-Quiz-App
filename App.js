d8ab72422aa2d24cf9829834c1d0ed80beb1eda1
programmer812 <64325806+programmer812@users.noreply.github.com>
2023-02-11 00:16:48 -0700

    App Setup

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
